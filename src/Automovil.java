9d9452f846e58f8af344d46fe026a95cbfddd70d
Mateo Torres <mateoch632@gmail.com>
2024-10-08 08:56:52 -0500

    Second Commit

--- src/Automovil.java
+++ src/Automovil.java
@@ -4,6 +4,7 @@ public class Automovil {
     //la clase principal de java es object
     //el bytecode esta en medio del programa fuente y el programa objeto
 
+
     private String marca;
     private String modelo;
     private String color;
@@ -17,6 +18,14 @@ public class Automovil {
     public Automovil() {
     }
 
+    public Automovil(String marca) {
+        this.marca = "bmw";
+    }
+
+    public Automovil(String marca, String modelo) {
+        this.marca= marca;
+        this.modelo = modelo;
+    }
 
     //metodos set y get
 
@@ -24,9 +33,9 @@ public class Automovil {
         return marca;
     }
 
-    public void setMarca(String marca) {
+    /*public void setMarca(String marca) {
         this.marca = marca;
-    }
+    }*/
 
     public String getCilindraje() {
         return cilindraje;
@@ -48,9 +57,9 @@ public class Automovil {
         return modelo;
     }
 
-    public void setModelo(String modelo) {
+   /* public void setModelo(String modelo) {
         this.modelo = modelo;
-    }
+    }*/
 
     //metodos propios y de calculo
 
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,22 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Automovil auto = new Automovil ();
+        Automovil auto = new Automovil ("bmw");
 
-        auto.setMarca("Mercedes");
+        //auto.setMarca("Mercedes");
         auto.setModelo("AMG");
         auto.setColor("Rojo");
         auto.setCilindraje("2000");
 
         System.out.println("Detalles del auto:\n" + auto.verDetalle());
+
+        Automovil auto2 = new Automovil("Mercedes","ML 200");
+        //auto2.setMarca("Mercedes");
+        //auto2.setModelo("AMG");
+        auto2.setColor("Rojo");
+        auto2.setCilindraje("2000");
+
+        System.out.println("Segundo auto\n " + auto2.verDetalle());
 
     }
 }
